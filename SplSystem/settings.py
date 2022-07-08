4b8eb535577d8a8262f7ccec4ec86a4f989029ee
Md Rayhan B. Khan <61648017+rayhanrock@users.noreply.github.com>
2022-07-09 00:33:29 +0600

    Update settings.py

--- SplSystem/settings.py
+++ SplSystem/settings.py
@@ -129,8 +129,8 @@ AUTH_USER_MODEL = 'users.UserProfile'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = "smtp.gmail.com"
-EMAIL_HOST_USER = 'emailmanager.yourjobaid@gmail.com'
-EMAIL_HOST_PASSWORD = 'ymzaqnpvmehqzddg'
+EMAIL_HOST_USER = '##'
+EMAIL_HOST_PASSWORD = '##'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
